99446b5c426d70fe7f7d93efeeaae476ceb8e496
kadir211 <yohansmillion@yahoo.com>
2021-07-29 22:52:16 -0700

    Added second update

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("about to share project on Github");
 
     }
 }
